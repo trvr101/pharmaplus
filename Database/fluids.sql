2f5f5dbb0b476118847b7ae60c64bb6783687d18
vhonabas <abasvhon20@gmail.com>
2023-11-17 07:58:43 -0400

    insert fluid sql

--- Database/fluids.sql
+++ Database/fluids.sql
@@ -1,5 +1,13 @@
 --oral tabs
 INSERT INTO item_db ( item_name, strength, category_id)
 VALUES
-('acetylcysteine sachet', '600mg', 1),
-('allopurinol tab', '100mg', 1),
+('D5IMB', '500ml', 6),
+('D10 Water', '500ml', 6),
+('D5NM', '1000ml', 6),
+('D5NR', '1000ml', 6),
+('D5Water', '250ml', 6),
+('D5Water', '500ml', 6),
+('IRRIGATING SOLUTION', '1000ml', 6),
+('Mannitol bottle', '500ml', 6),
+('PNSS', '1000ml', 6),
+('PLAIN LR', '1000ml', 6),
