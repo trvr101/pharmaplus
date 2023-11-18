3e893b01666e9f52f53a7964ac1cacc95bd8276e
cessangeles <130669646+cessangeles@users.noreply.github.com>
2023-11-18 10:42:27 +0800

    add dangerous_drugs

--- Database/dangerous_drugs.sql
+++ Database/dangerous_drugs.sql
@@ -1,5 +1,12 @@
---oral tabs
+--dangerous drugs
 INSERT INTO item_db ( item_name, strength, category_id)
 VALUES
-('acetylcysteine sachet', '600mg', 1),
-('allopurinol tab', '100mg', 1),
+('Diazepam amp', '5mg/ml(10mg/ml)', 5),
+('Ephedrine Sulphate amp', '50mg/ml', 5),
+('Midazolam ampule', '5mg/ml', 5),
+('Ketamine Hydrochloride amp', '50mg/ml', 5),
+('Morphine ampule', '10mg', 5),
+('Morphine tablet', '10mg', 5),
+('Nalbuphine HCL ampule', '10mg/1ml', 5),
+('Phenobarbital tab', '60mg', 5),
+('Phenobarbital tab', '30mg', 5),
