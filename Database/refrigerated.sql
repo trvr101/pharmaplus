a10ecf8a28bab122fad305233350f3139120afe6
cessangeles <130669646+cessangeles@users.noreply.github.com>
2023-11-19 14:44:39 +0800

    add refrigerated meds

--- Database/refrigerated.sql
+++ Database/refrigerated.sql
@@ -1,5 +1,16 @@
---oral tabs
+--refrigerated meds
 INSERT INTO item_db ( item_name, strength, category_id)
 VALUES
-('acetylcysteine sachet', '600mg', 1),
-('allopurinol tab', '100mg', 1),
+('ATS Tetanus Antitoxin ampule', '500IU/0.7ml', 3),
+('Atracurium Besilate ampule', '10mg/ml (2.5ml)', 3),
+('Bupivacaine Heavy (ER)', '5mg/ml', 3),
+('Epoetin Alfa vial', '4000 units/ml', 3),
+('Glycerol adult suppository', '2.5g', 3),
+('Human Albumin vial', '20% (5ml)', 3),
+('Methylergometrine amp', '200mcg/ml', 3),
+('Oxytocin ampule', '10IU/ml', 3),
+('Paracetamol Supp', '125mg', 3),
+('Paracetamol Supp', '250mg', 3),
+('Rocuronium Bromide', '10mg/ml (5ml)', 3),
+('Tetanus Toxoid', '40IU/0.5ml', 3),
+('Suxamethonium Chloride vial', '20mg/ml (10ml)', 3),
