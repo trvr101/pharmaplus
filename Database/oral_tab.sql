457346085d8452c185ebbcb37d6eb7e66f495ced
ravenvillapando <115708829+Krishn-Pesigan@users.noreply.github.com>
2023-11-28 02:13:21 +0800

    oral tab
    Co-Authored-By: Robert Joseph C. Aguba <144866148+trvr101@users.noreply.github.com>
    Co-Authored-By: cessangeles <130669646+cessangeles@users.noreply.github.com>
    Co-Authored-By: vhonabas <82793403+vhonabas@users.noreply.github.com>

--- Database/oral_tab.sql
+++ Database/oral_tab.sql
@@ -3,3 +3,27 @@ INSERT INTO item_db ( item_name, strength, category_id)
 VALUES
 ('acetylcysteine sachet', '600mg', 1),
 ('allopurinol tab', '100mg', 1),
+('aluminum magnesium tablet', '200mg/100mg', 1),
+('amlodipine besilate tablet', '10mg', 1),
+('amlodipine besilate tablet', '5mg', 1),
+('amoxicillin tablet', '500mg', 1),
+('ascorbic acid tablet', '500mg', 1),
+('aspirin tablet', '80mg', 1),
+('atorvastatin tablet', '20mg', 1),
+('atorvastatin tablet', '40mg', 1),
+('atorvastatin tablet', '80mg', 1).
+('azithromycin tablet', '500mg', 1),
+('betahistine tablet', '8mg', 1),
+('betahistine tablet', '16mg', 1),
+('bisacodyl tablet', '5mg', 1),
+('betahistine tablet', '24mg', 1),
+('budesonide neb', '500mcg/2ml', 1),
+('butamirate citrate tablet', '500mg', 1),
+('calcium carbonate tablet', '500mg', 1),
+('carvedilol tablet', '6.25mg', 1),
+('captopril tablet', '25mg', 1),
+('cefalexin cap', '500mg', 1),
+('cefixime capsule', '200mg', 1),
+('celecoxib capsule (ER)', '200mg', 1),
+('cetrizine tablet', '10mg', 1)
+
